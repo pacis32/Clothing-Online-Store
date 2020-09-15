63024223f4f86ca60c51fbcdf325d7a56d0be8fd
pacis32 <pacis32@gmail.com>
2020-09-15 16:00:44 -0500

    adding apicture

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import productReducers from './productReducer'
+import cartReducers from "./cartReducers";
+
+export default combineReducers({
+  products: productReducers,
+  cart: cartReducers,
+});
